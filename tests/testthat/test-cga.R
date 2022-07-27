a1ae321e2a5c42a0c869dfc11b07a1acb38be0ec
jbytecode <mhsatman@gmail.com>
2022-07-27 22:22:21 +0300

    update version, doc, and tests

--- tests/testthat/test-cga.R
+++ tests/testthat/test-cga.R
@@ -29,11 +29,14 @@ test_that("CGA - All 100 bits are 0s", {
 })
 
 test_that("CGA - Generate Chromosome", {
-    probs <- rep(0.5, 10)
-    v <- rep(0, 10)
-    cga_generate_chromosome(probs, v)
-    for (element in v) {
-        expect_gte(element, 0.0)
-        expect_lte(element, 1.0)
-    }
+    probs <- rep(0.5, 1000)
+    v <- rep(0, 1000)
+    result <- cga_generate_chromosome(probs, v)
+    
+    # Function returns NULL
+    expect_null(result)
+    
+    # Not all of the values are same
+    expect_false(all(v == 0))
+    expect_false(all(v == 1))
 })
